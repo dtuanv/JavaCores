1c6ef202c4f6678fe2a4fef002d31fcdacb14b0f
v.dao <dtuanlieb@gmail.com>
2022-11-24 17:03:48 +0100

    absolute Value Math function

---
+++ src/jvMath/AbsoluteValue.java
@@ -0,0 +1,7 @@
+package jvMath;
+
+public class AbsoluteValue {
+    public static void main(String[] args){
+        System.out.println("Absoluter Wert von - 152: "+Math.abs(-152));
+    }
+}
---
+++ src/jvMath/SquareRoot.java
@@ -0,0 +1,10 @@
+package jvMath;
+
+
+
+public class SquareRoot {
+    public static void main(String[] args){
+        int squareRootNum =  (int) Math.sqrt(16);
+        System.out.println("Der Quadratwurzel von 16 ist: "+squareRootNum);
+    }
+}
