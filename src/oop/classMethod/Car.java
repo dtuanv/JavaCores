4a558f254dcdb43f9dfb83358f5cfc04864c3d6a
v.dao <dtuanlieb@gmail.com>
2022-11-25 14:57:33 +0100

    oop

---
+++ src/oop/classMethod/Car.java
@@ -0,0 +1,14 @@
+package oop.classMethod;
+
+public class Car {
+    private String mark;
+
+    public void dive(){
+        System.out.println("The Car is running!");
+    }
+    public void carInfo(String color){
+        System.out.println("The Color of Car: "+color);
+    }
+
+
+}
